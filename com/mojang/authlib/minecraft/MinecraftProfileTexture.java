cddd8553cddd6cac505fa9c4aa5a7d55a7b34898
sasha0552 <admin@sasha0552.org>
2024-07-30 23:51:08 +0000

    authlib-1.5.17

--- com/mojang/authlib/minecraft/MinecraftProfileTexture.java
+++ com/mojang/authlib/minecraft/MinecraftProfileTexture.java
@@ -1,7 +1,11 @@
 package com.mojang.authlib.minecraft;
 
+import com.google.common.collect.Maps;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import javax.annotation.Nullable;
+import java.util.Map;
 
 public class MinecraftProfileTexture {
     public enum Type {
@@ -11,13 +15,23 @@ public class MinecraftProfileTexture {
     }
 
     private final String url;
+    private final Map<String, String> metadata;
 
-    public MinecraftProfileTexture(String url) {
+    public MinecraftProfileTexture(String url, Map<String, String> metadata) {
         this.url = url;
+        this.metadata = metadata;
     }
 
     public String getUrl() {
         return url;
+    }
+
+    @Nullable
+    public String getMetadata(String key) {
+        if (metadata == null) {
+            return null;
+        }
+        return metadata.get(key);
     }
 
     public String getHash() {
